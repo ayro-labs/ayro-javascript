864ea3336bdff658537146b707c5577f9c078823
Sandro Simas <sandro.csimas@gmail.com>
2018-04-30 09:39:46 -0300

    Improving css

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -57,11 +57,9 @@ class Chatbox extends React.Component<IStateProps & IDispatchProps, IState> {
           <div className="ayro-input">
             <input type="text" name="message" ref={this.setInputElement} placeholder={this.props.settings.chatbox.input_placeholder} value={this.state.message} onChange={this.onMessageChanged} onKeyPress={this.onKeyPress}/>
           </div>
-          <div onClick={this.postMessage}>
-            <svg className={this.iconSendClasses()} width="20" height="20" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z"/>
-            </svg>
-          </div>
+          <svg onClick={this.postMessage} className={this.iconSendClasses()} width="20" height="20" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z"/>
+          </svg>
         </div>
       </div>
     );
